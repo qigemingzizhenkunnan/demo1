41c89e315d7c4e37c3716ff33f03fd0703685912
xiaobo <1443745997@qq.com>
2020-02-05 15:39:52 +0800

    测试二

---
+++ src/com/bean/Hello.java
@@ -0,0 +1,12 @@
+package com.bean;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello java");
+        if (5>3){
+            System.out.println("5");
+        }else {
+            System.out.println("3");
+        }
+    }
+}
